ee764a02ed044397ed28846919b27ef28b145bf2
AarnaSingh <singh.aarna@gmail.com>
2020-07-14 16:49:06 +0530

    Added a platform

--- sketch.js
+++ sketch.js
@@ -18,6 +18,8 @@ function setup(){
     
     ground = new Ground(600,height,1200,20)
 
+  platform = new Ground(150,305,300,170);
+
     box1 = new Box(700,320,70,70);
     box2 = new Box(920,320,70,70);
     pig1 = new Pig(810, 350);
@@ -40,12 +42,10 @@ function setup(){
 function draw(){
     background(backgroundImg);
     Engine.update(engine);
-    console.log(box2.body.position.x);
-    console.log(box2.body.position.y);
-    console.log(box2.body.angle);
-    box1.display();
+      box1.display();
     box2.display();
     ground.display();
+    platform.display();
     pig1.display();
     log1.display();
 
